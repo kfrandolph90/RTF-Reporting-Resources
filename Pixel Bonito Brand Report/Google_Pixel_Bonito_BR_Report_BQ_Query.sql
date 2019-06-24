ed67859aad8a5c80d59fb9eeb3e3839b0bc23d5a
kyle.randolph <kyle.randolph@essenceglobal.com>
2019-06-24 19:49:28 -0400

    try to integrate YT reserve

--- Pixel Bonito Brand Report/Google_Pixel_Bonito_BR_Report_BQ_Query.sql
+++ Pixel Bonito Brand Report/Google_Pixel_Bonito_BR_Report_BQ_Query.sql
@@ -1,5 +1,6 @@
 /*
 Google Pixel Bonito Q2-Q3 - Report
+THIS IS A SHITTY QUERY
 */
 
   ------------------- Union All Video Tables -------------------
@@ -142,39 +143,93 @@ WITH
   GROUP BY
     1,
     2 ),
-  ------------------- DataLab Export -------------------
+  ------------------- Join Plan Line, aggregate and union onto YT pull -------------------
+  moat_vid_line as (  
+  select 
+  date,
+  olive_meta.Olive_Plan_Line_ID as plan_line_id,
+  sum(moat_vid_impressions_analyzed) as moat_vid_impressions_analyzed,
+  SUM(moat_vid_susp_valid) AS moat_vid_susp_valid,
+    SUM(valid_and_viewable) AS valid_and_viewable,
+    SUM(reached_first_quart_sum) reached_first_quart_sum,
+    SUM(reached_second_quart_sum) reached_second_quart_sum,
+    SUM(reached_third_quart_sum) reached_third_quart_sum,
+    SUM(reached_complete_sum) reached_complete_sum,
+    SUM(player_visible_on_complete_sum) player_visible_on_complete_sum,
+    SUM(player_audible_on_complete_sum) player_audible_on_complete_sum,
+    SUM(player_vis_and_aud_on_complete_sum) player_vis_and_aud_on_complete_sum,
+    SUM(susp_valid_and_inview_gm_meas_sum) susp_valid_and_inview_gm_meas_sum
+    from moat_vid
+    
+    LEFT JOIN  `essence-analytics-dwh.rtf_pixel_brand_report.STAGING_olive_plan_placement` olive_meta
+  on moat_vid.opid = olive_meta.Olive_Olive_Placement_ID
+    group by 1,2
+    
+    
+  UNION ALL 
+  
+  SELECT
+  date,
+  CASE
+    WHEN level2_id = 2520197687 THEN 84988
+    WHEN level2_id = 2549731844 THEN 84989
+    WHEN level2_id = 2550298543 THEN 85312
+    WHEN level2_id = 2547410762 THEN 84863
+END
+  AS plan_line_id,
+  SUM(impressions_analyzed) AS moat_vid_impressions_analyzed,
+  SUM(susp_valid) AS moat_vid_susp_valid,
+  SUM(valid_and_viewable) AS valid_and_viewable,
+  SUM(reached_first_quart_sum) reached_first_quart_sum,
+  SUM(reached_second_quart_sum) reached_second_quart_sum,
+  SUM(reached_third_quart_sum) reached_third_quart_sum,
+  SUM(reached_complete_sum) reached_complete_sum,
+  SUM(player_visible_on_complete_sum) player_visible_on_complete_sum,
+  SUM(player_audible_on_complete_sum) player_audible_on_complete_sum,
+  SUM(player_vis_and_aud_on_complete_sum) player_vis_and_aud_on_complete_sum,
+  SUM(susp_valid_and_inview_gm_meas_sum) susp_valid_and_inview_gm_meas_sum
+FROM
+  `essence-analytics-dwh.rtf_pixel_brand_report.STAGING_yt_res`
+GROUP BY
+  1,
+  2
+),
+    
+  ------------------- Join Plan Line and Aggregate Moat Disp -------------------
+  moat_disp_line as (
+  select 
+   date,
+  olive_meta.Olive_Plan_Line_ID as plan_line_id,
+  sum(moat_disp_impressions_analyzed) as moat_disp_impressions_analyzed,
+  sum(moat_dis_susp_valid) as moat_dis_susp_valid
+  from moat_disp
+  LEFT JOIN  `essence-analytics-dwh.rtf_pixel_brand_report.STAGING_olive_plan_placement` olive_meta
+  on moat_disp.opid = olive_meta.Olive_Olive_Placement_ID
+  group by 1,2),
+
+ ------------------- DataLab Export -------------------
   datalab AS (
   SELECT
     Time_1__Date_of_Activity AS date,
-    Olive_Plan_Name AS media_plan_name,
     Olive_Plan_Line_ID AS plan_line_id,
     Olive_Plan_Line_Name AS plan_line_name,
     Olive__Plan_Line_Property_ AS property,
     Olive_Plan_Line_Type AS type,
     Olive__Plan_Line_Channel_ AS channel,
-    Olive__Placement_ID_ AS opid,
-    Performance_And_Delivery_1__Impressions AS datalab_impressions,
-    Performance_And_Delivery_2__Clicks AS clicks,
-    Performance_And_Delivery_6__Spend_in_USD AS spend_usd
+    sum(Performance_And_Delivery_1__Impressions) AS datalab_impressions,
+    sum(Performance_And_Delivery_2__Clicks) AS clicks,
+    sum(Performance_And_Delivery_6__Spend_in_USD) AS spend_usd
   FROM
     `essence-analytics-dwh.rtf_pixel_brand_report.datalab_export`
   WHERE
-    Olive_Plan_Line_Name != "YouTube Masthead") -- Matt Doesn't care about this)
-  ------------------- Whole Shebang -------------------
-SELECT
-  *,
-  ifnull(moat_vid_impressions_analyzed,
-    0) + ifnull(moat_disp_impressions_analyzed,
-    0) AS total_moat_impressions_analyzed
-FROM
-  datalab
-LEFT JOIN
-  moat_disp
-USING
-  (date,
-    opid)
-LEFT JOIN
-  moat_vid
-USING
-  (date,
-    opid)
+    Olive_Plan_Line_Name != "YouTube Masthead"
+    group by 1,2,3,4,5,6) -- Matt Doesn't care about this)
+
+
+ ------------------- Whole Shebang -------------------
+ 
+ select *
+ from datalab 
+ left join moat_disp_line using(date,plan_line_id) 
+ left join moat_vid_line using(date,plan_line_id)
+ 
