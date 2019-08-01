06d7ccbc28638e70cebf46de8e695fbe1f8eff96
Noah-Hazan <52014504+Noah-Hazan@users.noreply.github.com>
2019-08-01 12:40:03 -0400

    adding new script

---
+++ Generic_Python_Scripts_And_Dags/generate_lookml_view_to_create_metadata.py
@@ -0,0 +1,60 @@
+label = ""
+relabel = ""
+
+
+# Will become metadataname_label and metadataname_relabel in Looker
+metadataname = 'pubs_final'
+
+
+# Must manually paste from sheets. Make sure there is no space between the open and end quotation marks.
+label = '''
+
+'''
+
+
+relabel = '''YouTube
+
+
+'''
+
+# Replace the end of the line with a comma, and then split on the comma.
+label = label.replace("\n",",")
+label=label.split(",")
+
+relabel = relabel.replace("\n",",")
+relabel=relabel.split(",")
+
+
+looker_head = ''' view : ''' + metadataname + '''_metadata {
+
+      derived_table: {
+
+        sql:  SELECT *
+      FROM (VALUES  '''
+
+
+sql_meat = ""
+for i in range(len(label)):
+    sql_meat += ("        (" + "'"+label[i]+"'" + "," + "'"+relabel[i]+"'" + ")")
+    if i != len(label)-1:
+        sql_meat += "," + "\r\n"
+
+sql_meat += (") t (" + metadataname + "_label, " + metadataname + "_relabel)")
+sql_meat += ";;"
+sql_meat += "}"
+
+
+looker_tail1 =  '''dimension: ''' +  metadataname + '''_label {
+type: string
+sql: ${TABLE}.''' + metadataname + '''_label''' +   ''';;''' + ''' } '''
+
+looker_tail2 = '''dimension: ''' +  metadataname + '''_relabel {
+type: string
+sql: ${TABLE}.''' + metadataname + '''_relabel''' + ''';;''' + ''' }    } '''
+
+print(looker_head)
+print(sql_meat)
+print("\r\n")
+print(looker_tail1)
+print("\r\n")
+print(looker_tail2)
